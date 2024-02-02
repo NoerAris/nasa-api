10bd0ae47d574f02a74d9522fe39c404257bfeab
aris <ariss2932@gmail.com>
2024-02-02 21:45:06 +0700

    add SwaggerConfig.java, CommonConstant.java, update pom.xml
    Signed-off-by: aris <ariss2932@gmail.com>

---
+++ src/main/java/com/nasa/api/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.nasa.api.config;
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@Profile({"!production"})
+public class SwaggerConfig {
+    @Bean
+    public Docket masterAssignment() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("NASA-Api-v1.0")
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                .build()
+                .apiInfo(new ApiInfoBuilder().version("v1").title("NASA API").description("NASA Open API Documentation").build());
+    }
+}
